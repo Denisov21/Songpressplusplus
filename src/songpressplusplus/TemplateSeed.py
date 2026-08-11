c6badc1e1afe898557b2450b337b1b373642a16f
Denis R <itis05@virgilio.it>
2026-08-11 19:27:53 +0200

    v7.0.2

--- src/songpressplusplus/TemplateSeed.py
+++ src/songpressplusplus/TemplateSeed.py
@@ -2,8 +2,9 @@
 # Name:         TemplateSeed.py
 # Purpose:      Popola la cartella template dell'utente copiandovi, al primo
 #               avvio, tutti i template distribuiti con il pacchetto.
-# Create by:  Denisov21
-# License:      GNU GPL v2
+# Created:     2026-08-11
+# Author by:   Denisov21
+# License:     GNU GPL v2
 ##############################################################
 """Seeding della cartella template dell'utente.
 
